5cc72207c4fa2c91b055f3ca78f9c4292f46f268
Ásmundur Óskar Ásmundsson <asmundur31@gmail.com>
2022-12-15 19:34:51 +0100

    Initial Commit

---
+++ MySportsRecordIOS/MySportsRecordIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySportsRecordIOSApp.swift
+//  MySportsRecordIOS
+//
+//  Created by Ásmundur Óskar Ásmundsson on 15.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MySportsRecordIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
